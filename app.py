b80e835686c7a872e2f3562d9fa5ddb1f4a23491
Romich <roman@softsystems.com.ua>
2018-06-05 17:28:26 +0300

    piu

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import requests
 from flask import Flask, redirect, request
 import os
 
-callbackurl = 'http://datarobottest.herokuapp.com/callback'
-redirecturl = 'http://datarobottest.herokuapp.com/auth/redirect'
 github_auth_url = 'https://github.com/login/oauth/authorize'
 github_api_url = 'https://api.github.com'
+redirect_url = 'http://datarobottest.herokuapp.com/auth/redirect'
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
-app = Flask('flask_name')
+app = Flask('datarobottest')
 
 
 @app.route('/')
@@ -19,45 +18,22 @@ def index():
 
 @app.route('/auth')
 def request_authorization():
-    return authorize()
-    #return(r.text)
-
-
-@app.route('/response')
-def handle_responce():
-    return('Response recieved')
-
-
-@app.route('/callback')
-def callback():
-    return('callback triggered \n %s' % request.headers)
-
-
-@app.route('/redirect/auth/', methods=['POST'])
-def redirect_auth():
-    token_request(request.values.get('authenticity_token'))
-    return('Redirect auth triggered <br />  %s <br />  %s' % (request.headers, requests.json) )
-
-
-@app.route('/session', methods=['POST'])
-def session():
-    token_request(request.values.get('authenticity_token'))
-    return('session triggered <br /> <br /> %s <br /> <br />  %s' % (request.headers, request.values) )
-
-
-def authorize():
-
-    github_url_params = '%s?client_id=%s&redirect_uri=%s' % (github_auth_url, client_id, redirecturl)
-    #parameters = {'client_id': client_id, 'redirect_uri': redirecturl}
-    #r = requests.get(github_url, params=parameters))
+    github_url_params = '%s?client_id=%s&redirect_uri=%s' % (github_auth_url, client_id, redirect_url)
     return redirect(github_url_params)
+
+
+@app.route('/auth/redirect', methods=['POST'])
+def redirect_auth():
+    responce_code = requests.json.get('code')
+    user_token = token_request(responce_code)
+    return('Redirect auth triggered <br />  %s <br />  Token len %s' % (request.headers, len(user_token)) )
 
 
 def token_request(token_code):
     token_url = 'https://github.com/login/oauth/access_token'
     parameters = {'client_id': client_id, 'client_secret': client_secret, 'code': token_code}
     r = requests.post(token_url, params=parameters)
-    print(r.text)
+    return r
 
 
 if __name__ == '__main__':
